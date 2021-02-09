f3ab1f9dcd027cb68fa44ca8aebe3824d48003b6
Ilya Makarevich <ilya.makarevich@apalon.com>
2021-02-09 12:47:43 +0300

    add project

---
+++ LearningSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LearningSwift
+//
+//  Created by Ilya Makarevich on 2/9/21.
+//
+
+import UIKit
+import RxSwift
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
