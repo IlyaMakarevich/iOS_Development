de021b9759211975a1d394b2ee2c6a9080f98716
Ilya Makarevich <ilya.makarevich@apalon.com>
2021-02-28 15:33:51 +0300

    lesson 6, single observable

--- LearningSwift/ViewController.swift
+++ LearningSwift/ViewController.swift
@@ -89,6 +89,66 @@ class ViewController: UIViewController {
 
         }
         
+        example(of: "create") {
+            
+            enum Droid: Error {
+                case OI812
+            }
+            let disposeBag = DisposeBag()
+            Observable<String>.create { observer in
+                observer.onNext("robot police")
+                observer.onError(Droid.OI812)
+                observer.onNext("robot police2")
+                observer.onNext("robot police3")
+                observer.onCompleted()
+                return Disposables.create()
+            }.subscribe(onNext: {print($0)}, onError: {print($0)}, onCompleted: {print("compl")}, onDisposed: {print("disposed")}).disposed(by: disposeBag)
+        }
+        
+        example(of: "Single") {
+            let disposedBag = DisposeBag()
+            
+            enum FileReadError: Error {
+                case fileNotFound, unreadable, encodingFailed
+            }
+            
+            
+            func loadText(from fileName: String) -> Single<String> {
+                return Single.create { single in
+                    let disposable = Disposables.create()
+                    
+                    guard let path = Bundle.main.path(forResource: fileName, ofType: "txt") else {
+                        single(.failure(FileReadError.fileNotFound))
+                        return disposable
+                    }
+                    
+                    guard let data = FileManager.default.contents(atPath: path) else {
+                        single(.failure(FileReadError.unreadable))
+                        return disposable
+                    }
+                    
+                    guard let contents = String(data: data, encoding: .utf8) else {
+                        single(.failure(FileReadError.encodingFailed))
+                        return disposable
+                    }
+                    
+                    single(.success(contents))
+                
+                    return disposable
+                }
+            }
+            
+            loadText(from: "file")
+                .subscribe {
+                    switch $0 {
+                    case .success(let string):
+                        print(string)
+                    case .failure(let error):
+                        print(error)
+                    }
+                }.disposed(by: disposedBag)
+        }
+        
     }
 
 
